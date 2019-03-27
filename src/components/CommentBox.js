ad4b9ba402ffa002914b3a86d91430bb83a2b459
Kyle Asaff <kyleasaff@pixleeteam.com>
2019-03-27 16:37:35 -0700

    more tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // TODO - Call an action creator and save the comment that got entered by the user
+
+    this.setState({ comment: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange}/>
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default CommentBox;
