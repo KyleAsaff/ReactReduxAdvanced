f967fccbbb9d63688eef4a8bad9b3545037641ff
Kyle Asaff <kyleasaff@pixleeteam.com>
2019-03-28 12:58:43 -0700

    moxios wait

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -26,9 +26,10 @@ it('can fetch a list of comments and display them', (done) => {
   // find the fetch comment button and click it
   wrapped.find('.fetch-comments').simulate('click');
   // expect to find a list of comments
-  setTimeout(() => {
+  moxios.wait(() => {
     wrapped.update()
     expect(wrapped.find('li').length).toEqual(2);
     done();
-  }, 100);
+    wrapped.unmount();
+  });
 });
