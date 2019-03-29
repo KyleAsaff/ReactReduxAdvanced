873f2eaff65c67cd02e4f2f06b3db4af15463e9d
Kyle Asaff <kyleasaff@pixleeteam.com>
2019-03-29 12:18:17 -0700

    start writing middleare

---
+++ src/middlewares/async.js
@@ -0,0 +1,15 @@
+
+
+
+// Equivilant to the function wrapper below
+// export default function({ dispatch }) {
+//   return function(next) {
+//     return function(action) {
+//
+//     }
+//   }
+// }
+
+export default ({ dispatch }) => next => action => {
+
+}
