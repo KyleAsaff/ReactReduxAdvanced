a6cfec98449eee851a468a7ca8b32772be730e2e
Kyle Asaff <kyleasaff@pixleeteam.com>
2019-03-29 17:41:13 -0700

    server done

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,16 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  // user alread had their email and password authed we just need to give them a token
+  res.send({token: tokenForUser(req.user)});
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -22,8 +34,7 @@ exports.signup = function(req, res, next) {
 
   user.save(function(err) {
     if (err) { return next(err); }
+    res.json({ token: tokenForUser(user) });
   });
-
-  res.json(user);
 
 }
